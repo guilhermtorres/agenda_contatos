6049a9cc65ab33d572efbbe7d12eb4466153143d
Guilherme Torres <jguilherme.torres@gmail.com>
2020-05-13 23:59:40 -0300

    Estrutura de projeto montada

---
+++ lib/App/app.dart
@@ -0,0 +1,11 @@
+import 'package:agenda_contatos/App/src/views/home_views.dart';
+import 'package:flutter/material.dart';
+
+class AgendaContatosApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeViews(),
+    );
+  }
+}
---
+++ lib/App/src/views/home_views.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class HomeViews extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
