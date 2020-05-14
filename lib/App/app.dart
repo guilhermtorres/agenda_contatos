fb10f1385d1d6146e6da02391b35d2a021676455
Guilherme Torres <jguilherme.torres@gmail.com>
2020-05-14 00:54:22 -0300

    Aplicação e definição de tema

--- lib/App/app.dart
+++ lib/App/app.dart
@@ -6,6 +6,41 @@ class AgendaContatosApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomeViews(),
+      theme: ThemeData(
+        cursorColor: Colors.brown,
+        buttonColor: Colors.brown,
+        indicatorColor: Colors.brown,
+        accentColor: Colors.grey[700],
+        hintColor: Colors.brown,
+        primarySwatch: Colors.brown,
+        primaryColor: Colors.brown,
+        fontFamily: 'Amiri',
+        canvasColor: Colors.yellow[50],
+        textTheme: TextTheme(
+          bodyText2: TextStyle(
+            fontFamily: 'Amiri',
+            color: Colors.white,
+            fontSize: 24,
+          ),
+          headline6: TextStyle(
+            fontFamily: 'AsapCondensed',
+            color: Colors.white,
+            fontSize: 24,
+          ),
+        ),
+        appBarTheme: AppBarTheme(
+          elevation: 5,
+          color: Colors.brown,
+          iconTheme: IconThemeData(color: Colors.white),
+          textTheme: TextTheme(
+            headline6: TextStyle(
+              fontFamily: 'AsapCondensed',
+              color: Colors.white,
+              letterSpacing: 3,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
